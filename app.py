eac584990031bfbb836c1dbf3349005e590a41ac
Dmitry Aladin <anon.digriz@gmail.com>
2022-03-26 14:51:47 +0300

    Навигация между страницами

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route('/recognize', methods=('GET', 'POST'))
+def recognize():
+    return render_template("recognize.html")
